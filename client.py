e92d196347475d16030c78599b12a8b6180ed1aa
abdulatif-fs <fasijardiq@gmail.com>
2022-11-05 17:36:11 +0700

    first commit

---
+++ client.py
@@ -0,0 +1,36 @@
+from http import client
+from pydoc import cli
+import socket
+import select
+import sys
+import threading
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+nickname = input("masukan nickname anda: ")
+ip = "127.0.0.1"
+port = 8080
+client.connect((ip, port))
+
+def receive():
+    while True:
+        try:
+            pesan = client.recv(1024).decode('ascii')
+            if pesan == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(pesan)
+        except:
+            print(f'erorr sodaraa!!!')
+            client.close()
+            break
+
+def write():
+    while True:
+        pesan = f'{nickname} : {input("")}'
+        client.send(pesan.encode('ascii'))
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
---
+++ server.py
@@ -0,0 +1,61 @@
+from concurrent.futures import thread
+import socket
+import socketserver
+from _thread import *
+import sys
+import select
+import threading
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+
+ip = "127.0.0.1"
+port = 8080
+
+server.bind((ip, port))
+server.listen(5)
+print( '%s is Activated ...' % ip)
+print('Listening on port %s ...' % port)
+clients = []
+nicknames =[]
+
+def broadcast(pesan):
+    for client in clients:
+        client.send(pesan)
+
+def handle(client):
+    while True:
+        try:
+            pesan = client.recv(1024)
+            broadcast(pesan)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast(f'{nickname} keluar dari room! '.encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f'{str(address)} Terhubung!')
+
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f'nickname client: {nickname}')
+        client.send(f'terhubung kedalam server!'.encode('ascii'))
+        broadcast(f'{nickname} join the chat!'.encode('ascii'))
+        
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+receive()
